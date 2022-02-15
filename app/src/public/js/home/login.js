252149307523462ed67e531a711c191d4f2e5e71
양홍열 <drrd100@naver.com>
2022-02-15 23:18:21 +0900

    dom save

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,16 @@
-console.log("111");
+const id = document.querySelector("#id"),
+    password = document.querySelector('#password'),
+    loginBtn = document.querySelector("#loginBtn");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    console.log("클릭");
+
+    const req = {
+        id: id.value,
+        password: password.value,
+    }
+
+    console.log(req);
+}
