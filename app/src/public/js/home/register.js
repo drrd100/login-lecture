4e39d7dc1b8b7e195e87ce44f12b09551d110638
양홍열 <drrd100@naver.com>
2022-02-17 01:44:30 +0900

    회원가입 라우팅 구현

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,39 @@
+const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
+    password = document.querySelector('#password'),
+    confirmPassword = document.querySelector('#confirm-password'),
+    registerBtn = document.querySelector("#registerBtn");
+
+    console.log("hi");
+registerBtn.addEventListener("click", register);
+
+
+function register() {
+    if (!id.value) return alert("아이디를 입력해주십시요");
+    if (password !== confirmPassword) return alert("비밀번호가 일치하지 않습니다.");
+
+    const req = {
+        id: id.value,
+        name: name.value,
+        password: password.value,
+    };
+    console.log(req);
+    fetch("./register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/login";
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+        console.error(new Error("회원가입 중 에러 발생"));
+        })
+}
