ae29d2f3a1e773bb94eb3cd5ad7120cdaf57e984
양홍열 <drrd100@naver.com>
2022-02-16 21:06:55 +0900

    make login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -9,11 +9,23 @@ function login() {
         id: id.value,
         password: password.value,
     }
+    console.log(req);
     fetch("./login", {
         method: "POST",
         headers: {
-            "Content-Type": "appliction/json",
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/";
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+            console.error(new Error("로그인 중 에러 발생"));
+        });
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,12 +1,40 @@
-const home = (req, res) => {
-    res.render('home/index');
-}
+const output = {
+    home: (req, res) => {
+        res.render('home/index');
+    },
+    login: (req, res) => {
+        res.render('home/login');
+    }
+}; 
 
-const login = (req, res) => {
-    res.render('home/login');
-}
+const users = {
+    id: ["test1", "test2", "test3"],
+    password: ["1", "2", "3"],
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            password = req.body.password;
+        
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.password[idx] === password) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다",
+        })
+    },
+};
+
 
 module.exports = {
-    home,
-    login
+    output,
+    process,
 }
